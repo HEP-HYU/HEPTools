7ff93f9c04aba3b2799736a4742c83df1cff6dba
TAE JEONG KIM <monttj@gmail.com>
2017-02-15 00:36:21 +0900

    first version of macro for plots

---
+++ MyAnalysis.C
@@ -0,0 +1,211 @@
+#define MyAnalysis_cxx
+// The class definition in MyAnalysis.h has been generated automatically
+// by the ROOT utility TTree::MakeSelector(). This class is derived
+// from the ROOT class TSelector. For more information on the TSelector
+// framework see $ROOTSYS/README/README.SELECTOR or the ROOT User Manual.
+
+
+// The following methods are defined in this file:
+//    Begin():        called every time a loop on the tree starts,
+//                    a convenient place to create your histograms.
+//    SlaveBegin():   called after Begin(), when on PROOF called only on the
+//                    slave servers.
+//    Process():      called for each event, in this function you decide what
+//                    to read and fill your histograms.
+//    SlaveTerminate: called at the end of the loop on the tree, when on PROOF
+//                    called only on the slave servers.
+//    Terminate():    called at the end of the loop on the tree,
+//                    a convenient place to draw/fit your histograms.
+//
+// To use this file, try the following session on your Tree T:
+//
+// root> T->Process("MyAnalysis.C")
+// root> T->Process("MyAnalysis.C","some options")
+// root> T->Process("MyAnalysis.C+")
+//
+
+
+#include "MyAnalysis.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TLorentzVector.h>
+
+void MyAnalysis::Begin(TTree * /*tree*/)
+{
+   // The Begin() function is called at the start of the query.
+   // When running with PROOF Begin() is only called on the client.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+
+   TString option = GetOption();
+
+}
+
+void MyAnalysis::SlaveBegin(TTree * /*tree*/)
+{
+   // The SlaveBegin() function is called after the Begin() function.
+   // When running with PROOF SlaveBegin() is called on each slave server.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+
+   TString option = GetOption();
+   cout << "SlaveBegin" << endl;
+   for(int ich=0; ich < 2; ich++){
+     for(int i=0; i < 4; i++){
+     
+       h_NJet[ich][i] = new TH1D(Form("h_NJet_Ch%i_S%i_%s",ich,i,option.Data()), "Number of jets", 14, 0, 14);
+       h_NJet[ich][i]->SetXTitle("Jet Multiplicity");
+       h_NJet[ich][i]->Sumw2();
+       fOutput->Add(h_NJet[ich][i]);
+     
+       h_NBJet_CSVv2M[ich][i] = new TH1D(Form("h_NBJet_CSVv2M_Ch%i_S%i_%s",ich,i,option.Data()), "Number of b tagged jets (medium)", 6, 0, 6);
+       h_NBJet_CSVv2M[ich][i]->SetXTitle("b-tagged Jet Multiplicity (CSVv2M)");
+       h_NBJet_CSVv2M[ich][i]->Sumw2();
+       fOutput->Add(h_NBJet_CSVv2M[ich][i]);
+
+       h_NBJet_CSVv2T[ich][i] = new TH1D(Form("h_NBJet_CSVv2T_Ch%i_S%i_%s",ich,i,option.Data()), "Number of b tagged jets (tight)", 6, 0, 6);
+       h_NBJet_CSVv2T[ich][i]->SetXTitle("b-tagged Jet Multiplicity (CSVv2T)");
+       h_NBJet_CSVv2T[ich][i]->Sumw2();
+       fOutput->Add(h_NBJet_CSVv2T[ich][i]);
+
+       h_NCJet_M[ich][i] = new TH1D(Form("h_NCJet_M_Ch%i_S%i_%s",ich,i,option.Data()), "Number of c tagged jets", 6, 0, 6);
+       h_NCJet_M[ich][i]->SetXTitle("c-tagged Jet Multiplicity (M)");
+       h_NCJet_M[ich][i]->Sumw2();
+       fOutput->Add(h_NCJet_M[ich][i]);  
+
+       h_MET[ich][i] = new TH1D(Form("h_MET_Ch%i_S%i_%s",ich,i,option.Data()), "MET", 200,0,400);
+       h_MET[ich][i]->SetXTitle("MET (GeV)");
+       h_MET[ich][i]->Sumw2();
+       fOutput->Add(h_MET[ich][i]);
+
+     }
+   }
+} 
+
+Bool_t MyAnalysis::Process(Long64_t entry)
+{
+   // The Process() function is called for each entry in the tree (or possibly
+   // keyed object in the case of PROOF) to be processed. The entry argument
+   // specifies which entry in the currently loaded tree is to be processed.
+   // When processing keyed objects with PROOF, the object is already loaded
+   // and is available via the fObject pointer.
+   //
+   // This function should contain the \"body\" of the analysis. It can contain
+   // simple or elaborate selection criteria, run algorithms on the data
+   // of the event and typically fill histograms.
+   //
+   // The processing can be stopped by calling Abort().
+   //
+   // Use fStatus to set the return value of TTree::Process().
+   //
+   // The return value is currently not used.
+
+   fReader.SetEntry(entry);
+
+   int mode = 999; 
+   mode = *channel;
+
+   if( mode > 2) return kTRUE;
+
+   float genweight = *genWeight;
+   float puweight = PUWeight[0];
+   float EventWeight = puweight*genweight;
+
+   //Object selection
+   int njets = 0;
+   int nbjets_m = 0; 
+   int nbjets_t = 0; 
+   int ncjets_m = 0; 
+
+   TLorentzVector lepton;
+   lepton.SetPtEtaPhiE(*lepton_pT, *lepton_eta, *lepton_phi, *lepton_E);
+
+   //Event selection 
+   bool passmuon = (mode == 0) && (lepton.Pt() > 30);
+   bool passelectron = (mode == 1) && (lepton.Pt() > 35);
+
+   if( passmuon ){
+
+     for (unsigned int iJet = 0; iJet < jet_pT.GetSize() ; ++iJet) {
+
+       TLorentzVector jet;
+       jet.SetPtEtaPhiE(jet_pT[iJet], jet_eta[iJet], jet_phi[iJet], jet_E[iJet]);
+
+       if( jet.Pt() > 30 ){
+         njets++;
+         if( jet_CSV[iJet] > 0.8484 )  nbjets_m++;
+         if( jet_CSV[iJet] > 0.9535 )  nbjets_t++;
+         if( jet_CvsL[iJet] > -0.1 && jet_CvsL[iJet] > 0.08 )  ncjets_m++;
+       }
+
+     }  
+
+     h_NJet[mode][0]->Fill(njets, EventWeight);
+     h_NBJet_CSVv2M[mode][0]->Fill(nbjets_m, EventWeight);
+     h_NBJet_CSVv2T[mode][0]->Fill(nbjets_t, EventWeight);
+     h_NCJet_M[mode][0]->Fill(ncjets_m, EventWeight);
+     h_MET[mode][0]->Fill(*MET, EventWeight);
+
+     if( njets >= 6) {
+       h_NJet[mode][1]->Fill(njets, EventWeight);
+       h_NBJet_CSVv2M[mode][1]->Fill(nbjets_m, EventWeight);
+       h_NBJet_CSVv2T[mode][1]->Fill(nbjets_t, EventWeight);
+       h_NCJet_M[mode][1]->Fill(ncjets_m, EventWeight);
+       h_MET[mode][1]->Fill(*MET, EventWeight);
+
+       if( nbjets_m >=2 ){
+         h_NJet[mode][2]->Fill(njets, EventWeight);
+         h_NBJet_CSVv2M[mode][2]->Fill(nbjets_m, EventWeight);
+         h_NBJet_CSVv2T[mode][2]->Fill(nbjets_t, EventWeight);
+         h_NCJet_M[mode][2]->Fill(ncjets_m, EventWeight);
+         h_MET[mode][2]->Fill(*MET, EventWeight);
+       }
+
+       if( nbjets_t >=2 ){
+         h_NJet[mode][3]->Fill(njets, EventWeight);
+         h_NBJet_CSVv2M[mode][3]->Fill(nbjets_m, EventWeight);
+         h_NBJet_CSVv2T[mode][3]->Fill(nbjets_t, EventWeight);
+         h_NCJet_M[mode][3]->Fill(ncjets_m, EventWeight);
+         h_MET[mode][3]->Fill(*MET, EventWeight);
+       }
+   
+     }
+
+   }
+
+   return kTRUE;
+}
+
+void MyAnalysis::SlaveTerminate()
+{
+   // The SlaveTerminate() function is called after all entries or objects
+   // have been processed. When running with PROOF SlaveTerminate() is called
+   // on each slave server.
+
+   TString option = GetOption();
+
+}
+   
+
+void MyAnalysis::Terminate()
+{
+   // The Terminate() function is the last function to be called during
+   // a query. It always runs on the client, it can be used to present
+   // the results graphically or save the results to file.
+   TString option = GetOption();
+
+   TFile * out = TFile::Open(Form("hist_%s.root",option.Data()),"RECREATE");
+
+   for(int ich = 0; ich < 2; ich++){
+     for(int i = 0; i < 4; i++){
+       fOutput->FindObject(Form("h_NJet_Ch%i_S%i_%s",ich,i,option.Data()))->Write();
+       fOutput->FindObject(Form("h_NBJet_CSVv2M_Ch%i_S%i_%s",ich,i,option.Data()))->Write();
+       fOutput->FindObject(Form("h_NBJet_CSVv2T_Ch%i_S%i_%s",ich,i,option.Data()))->Write();
+       fOutput->FindObject(Form("h_NCJet_M_Ch%i_S%i_%s",ich,i,option.Data()))->Write();
+     }
+   }
+
+   //fOutput->FindObject("ttbbLepJets/EventInfo")->Write();
+
+   out->Write();
+   out->Close();
+
+}
---
+++ makePlots.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python
+
+from ROOT import TStyle, TF1, TFile, TCanvas, gDirectory, TTree, TH1F, TH2F, THStack, TLegend, gROOT 
+import ROOT
+
+from style import *
+
+from collections import OrderedDict
+datasamples=OrderedDict()
+bkgsamples=OrderedDict()
+sigsamples=OrderedDict()
+
+def SetData(file, name, lumi):
+  tmp = {}
+  f = TFile(file)
+  fnames = f.GetName().split('.')
+  fname = fnames[0]
+
+  tmp["file"] = f
+  tmp["hname"] = [x.GetName() for x in f.GetListOfKeys()]
+  tmp["hname"].remove("EventInfo")
+  tmp["lumi"] = lumi 
+  tmp["name"] = name
+  datasamples[fname] = tmp
+ 
+def AddBkg(file, name, color, xsection):
+  tmp = {}
+  f = TFile(file)
+  fnames = f.GetName().split('.')
+  fname = fnames[0]
+ 
+  tmp["file"] = f
+  tmp["hname"] = [x.GetName() for x in f.GetListOfKeys()]
+  tmp["hname"].remove("EventInfo")
+
+  #debug
+  #N_h = len(tmp["hname"])
+  tmp_h = f.Get(tmp["hname"][0])
+  tmp_n = tmp_h.Integral()
+  print fname, " : ", name, " : ", tmp["hname"][0], " : " , tmp_n
+
+  h = f.Get("EventInfo")
+  nevt = h.GetBinContent(2)
+  tmp["total"] = nevt 
+  tmp["col"] = color
+  tmp["xsection"] = xsection
+  tmp["name"] = name
+  bkgsamples[fname] = tmp
+
+####Users should provide these information 
+SetData("hist_DataSingleMu.root","data", 35867) # for now, combination of muon and electron
+AddBkg("hist_ttbb.root","ttbb",ROOT.kRed+4, 356.4)
+AddBkg("hist_ttbj.root","ttbj",ROOT.kRed+3, 356.4)
+AddBkg("hist_ttcc.root","ttcc",ROOT.kRed+2, 356.4)
+AddBkg("hist_ttLF.root","ttLF",ROOT.kRed, 356.4)
+AddBkg("hist_tt.root","ttLF",ROOT.kRed, 356.4)
+AddBkg("hist_ttBkg.root","ttLF",ROOT.kRed, 831.8)
+AddBkg("hist_wjets.root","WJets",ROOT.kYellow,61524)
+AddBkg("hist_zjets.root","ZJets",ROOT.kBlue, 6025.2)
+AddBkg("hist_zjets10to50.root","ZJets",ROOT.kBlue, 18610.0)
+AddBkg("hist_tchannel.root","Single t",ROOT.kRed-4, 44.33)
+AddBkg("hist_tbarchannel.root","Single t",ROOT.kRed-4, 26.38)
+AddBkg("hist_tWchannel.root","Single t",ROOT.kRed-4, 35.6)
+AddBkg("hist_tbarWchannel.root","Single t",ROOT.kRed-4, 35.6)
+AddBkg("hist_ww.root","DiBoson",ROOT.kCyan, 118.7)
+AddBkg("hist_wz.root","DiBoson",ROOT.kCyan, 47.13)
+AddBkg("hist_zz.root","DiBoson",ROOT.kCyan, 16.523)
+#### 
+
+N_hist = len(datasamples[datasamples.keys()[0]]["hname"])
+N_bkgsamples = len(bkgsamples)
+
+for i in range(0, N_hist):
+  hs = THStack()
+  #l = TLegend(0.30, 0.99 - 0.8 * N_bkgsamples / 20., 0.89, 0.85)
+  l = TLegend(0.15,0.71,0.89,0.87)
+  l.SetNColumns(4);
+  l.SetTextSize(0.04);
+  l.SetLineColor(0);
+  l.SetFillColor(0);
+
+  ntotalbkg = 0
+  k = 0
+  for fname in bkgsamples.keys():
+    h_tmp = bkgsamples[fname]["file"].Get(bkgsamples[fname]["hname"][i])
+    h_tmp.SetFillColor(bkgsamples[fname]["col"])
+    ## check if the sample is the same as previous process. 
+    if k < N_bkgsamples-1 :
+      post_name = bkgsamples.keys()[k+1]
+      if bkgsamples[fname]["name"] is bkgsamples[post_name]["name"]:
+        bkgsamples[fname]["file"].Get(bkgsamples[fname]["hname"][i]).SetLineColor(bkgsamples[fname]["col"]) 
+      else:
+        l.AddEntry(h_tmp, bkgsamples[fname]["name"]  ,"F") 
+    else: 
+      l.AddEntry(h_tmp, bkgsamples[fname]["name"]  ,"F")
+    ## normalization
+    scale = datasamples[datasamples.keys()[0]]["lumi"]/(bkgsamples[fname]["total"]/bkgsamples[fname]["xsection"])
+    h_tmp.Scale(scale)
+    numevt = h_tmp.Integral()
+    rawevt = h_tmp.GetEntries()
+    ntotalbkg = ntotalbkg + numevt
+    print fname, " : ", bkgsamples[fname]["name"], " = ", "{0:.6g}".format(numevt), " raw : ", "{0:.1g}".format(rawevt), " scale : " ,"{0:.1g}".format(scale)  
+    hs.Add( h_tmp )
+    k = k+1 
+
+  c = TCanvas("c_"+"{}".format(i),"c",1)
+  h_data = datasamples[datasamples.keys()[0]]["file"].Get(datasamples[datasamples.keys()[0]]["hname"][i])
+  h_data.SetMarkerStyle(20)
+  h_data.SetMarkerSize(0.3)
+  max_data = h_data.GetMaximum()
+  max_hs = hs.GetMaximum()
+  if max_hs > max_data :
+    h_data.SetMaximum(max_hs+max_hs*0.5)
+  else:
+    h_data.SetMaximum(max_data+max_data*0.5)
+  h_data.Draw("p")
+  h_data.SetTitle("")
+  h_data.GetYaxis().SetTitle("Entries")
+  hs.Draw("histsame")
+  h_data.Draw("psame")
+
+  l.AddEntry(h_data,"Data","P")
+  l.Draw()
+  label = TPaveText()
+  label.SetX1NDC(gStyle.GetPadLeftMargin())
+  label.SetY1NDC(1.0-gStyle.GetPadTopMargin())
+  label.SetX2NDC(1.0-gStyle.GetPadRightMargin())
+  label.SetY2NDC(1.0)
+  label.SetTextFont(42)
+  label.AddText("CMS, 35.9 fb^{-1} at #sqrt{s} = 13 TeV")
+  label.SetFillStyle(0)
+  label.SetBorderSize(0)
+  label.SetTextSize(0.04)
+  label.SetTextAlign(32)
+  label.Draw("same")
+
+  ndata = h_data.Integral()
+  print "ntotal = " , "{0:.6g}".format(ntotalbkg)
+  print "ndata = " , "{0:.0f}".format(ndata)
+
+  c.Print("c_"+datasamples[datasamples.keys()[0]]["hname"][i]+".pdf")
+
